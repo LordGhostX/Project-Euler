56f08ec0dd8cd08479274b17c8af40a1a437737d
LordGhostX <solomonghost9@gmail.com>
2019-11-06 02:36:34 +0000

    added solution 16

---
+++ Solutions/solution_016.py
@@ -0,0 +1,11 @@
+"""Problem 16: Power digit sum
+
+2 ** 15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2 ** 1000?"""
+
+limit = 1000
+
+sum_num = sum(list(map(int, list(str(2 ** limit)))))
+
+print(sum_num)
